c2ec3ee77d0b751e0ef08e6f9a97eddb15328f8f
kitpao <kitpao91@gmail.com>
2020-05-07 17:18:41 -0500

    :sparkles: Introducing validations for user_name in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :user_name, uniqueness: true, length: { in: 6..15 }
+  validate :username_cant_be_nil
+
+  def username_cant_be_nil
+    errors.add(:user_name, 'you have to write something!')  if user_name.nil?
+  end
 end
