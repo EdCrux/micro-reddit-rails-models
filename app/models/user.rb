7dea0f627cb67817e4172e7f05e032c4d5fdc384
kitpao <kitpao91@gmail.com>
2020-05-08 12:09:14 -0500

    :sparkles: Introduce comments model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-
   has_many :posts
+  has_many :comments
   validates :user_name, uniqueness: true, length: { in: 6..15 }
   validate  :username_cant_be_nil
-  
+
   validates :password, length: {
    minimum: 6,
    maximum: 15
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_225121) do
+ActiveRecord::Schema.define(version: 2020_05_08_170254) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -29,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_05_07_225121) do
     t.string "password"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
